aa73f2c0c036ade9809b4d48cfb1862ccd3f5d60
Lopes, Andre <andre.lopes@exxonmobil.com>
2017-10-09 13:29:28 -0300

    created actions and body of reducers

---
+++ src/app/auth/store/auth.reducers.ts
@@ -0,0 +1,15 @@
+
+
+export interface State {
+  token: string;
+  authenticated: boolean;
+}
+
+const initialState: State = {
+  token: null,
+  authenticated: false
+};
+
+export function AuthReducer(state = initialState, action) {
+  return state;
+}
